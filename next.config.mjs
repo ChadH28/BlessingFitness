6481dd369db359a5c5d5ac04936105212068f233
Chad Hoosain <chad@bnry.digital>
2025-03-20 20:48:20 +0200

    deleted unused

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   output: "export",
   trailingSlash: true, 
-  basePath: "/BlessingFitness", // Must match the GitHub repo name exactly
-  assetPrefix: "/BlessingFitness/", // Ensures assets load correctly
+  // basePath: "/", // Must match the GitHub repo name exactly
+  // assetPrefix: "/", // Ensures assets load correctly
   images: {
     unoptimized: true, // Required for Next.js static export
   },
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,9 @@
 import Navigation from "@/components/navigation";
-import "./globals.css";
+import "@/app/globals.css";
 import Footer from "@/components/footer";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import AOSProvider from "@/components/aos";
-
-
 
 export default function RootLayout({ children }) {
   return (
